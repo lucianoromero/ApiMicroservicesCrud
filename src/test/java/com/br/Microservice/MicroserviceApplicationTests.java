2c786e06404c68bd5a58727e2dd87783d280225f
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-12-13 10:44:01 -0300

    01 Implementação inicial do projeto
    Criação do projeto

---
+++ src/test/java/com/br/Microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
